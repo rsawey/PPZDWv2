92e3943b933884fa1d9acf205ec1b804e3e6e223
Richard Sawey <richard.sawey@zuora.com>
2015-05-26 03:16:47 +0000

    getting rid of bogus dates file, just need date_ins.sql, 20150525

--- dates.sql
+++
@@ -1,6 +0,0 @@
-create table dates (yearmonthday real);
-insert into dates values ('2455198.5');
-insert into dates values ('2455199.5');
-insert into dates values ('2455197.5');
-insert into dates values ('2455197.5' + 10);
-select date(yearmonthday) from dates;
